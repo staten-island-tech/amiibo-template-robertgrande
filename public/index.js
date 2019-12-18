550f5b1ff3635704cb2584f80549dbce536989cc
Rob Grande <robert.grande@sitechhs.com>
2019-12-18 11:59:04 -0500

    OD AF but it works

--- public/index.js
+++ public/index.js
@@ -1,28 +1,49 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const hbs = require("hbs");
+const express = require("express");
 const app = express();
 
 //define paths for express
-const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates/views');
+const publicDirectoryPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../templates/partials");
+const viewsPath = path.join(__dirname, "../templates/views");
 
-//set up handlebars engine and views location
-app.set('views engine', 'hbs'); //tell express/NODE to use handelbars as a template
-app.set('views', viewsPath); //telling express to get template from template/views folder
+//setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath); //telling express to get templates from templates/views folder
 
-app.get('', async (req, res) => {
-    try{
-        res.render('index');
-    }catch(error){
-        res.status(500).send();
-    }
+hbs.registerPartials(partialsPath);
+
+app.get("", async (req, res) => {
+  try {
+    res.render("index", {
+      title: "Our First Express App"
+    });
+  } catch {
+    res.status(500).send();
+  }
 });
 
-app.get('/thamisucks', (req, res) => {
-    res.send('he succ. we habe to do wulik ate');
-}); //app.get is one of many requests that can be used for servers
+app.get("/about/:id", async (req, res) => {
+  const name = req.params.id;
+  try {
+    res.render("index", {
+      title: name
+    });
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.get("/anything", async (req, res) => {
+  try {
+    res.render("swag");
+  } catch {
+    res.status(500).send();
+  }
+});
 
 app.listen(3000, () => {
-    console.log('listen on port 3000');
-    console.log(__dirname);
+  console.log("Listening on port 3000");
+  console.log(viewsPath);
 });
