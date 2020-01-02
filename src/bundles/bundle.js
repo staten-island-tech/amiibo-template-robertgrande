4106c27dbe97c4b69fa000d2569155727d9a5d26
Rob Grande <robert.grande@sitechhs.com>
2020-01-02 11:46:33 -0500

    webpack work and node is hard

--- src/bundles/bundle.js
+++ src/bundles/bundle.js
@@ -1,15 +1,35 @@
-import {elements} from './elements';
+import { elements } from "./elements";
 
-elements.amiiboForm.addEventListener("keypress", function(e){
-    if(e.keyCode === 13){
-        e.preventDefault();
-        document.querySelector(".btn").click();
-    }
+elements.amiiboForm.addEventListener("keypress", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector(".btn").click();
+  }
 });
 
-function search(){
-    elements.amiiboForm.addEventListener("submit", async function(e){
-        e.preventDefault();
-        const amiiboURL = `https://www.amiiboapi.com/api/amiibo/?character=${elements.amiiboName.value}`;
-    });
+function search() {
+  elements.amiiboForm.addEventListener("submit", async function(e) {
+    e.preventDefault();
+    const amiiboUrl = `https://www.amiiboapi.com/api/amiibo/?character=${elements.amiiboName.value}`;
+    try {
+      const result = await fetch(amiiboUrl);
+      const data = await result.json();
+      data.amiibo.forEach(element => {
+        elements.displayArea.insertAdjacentHTML(
+          "beforeend",
+          `<ul class="display-amiibo">
+          <li class="display-name">${element.character}</li>
+          <li class="display-series">${element.amiiboSeries}</li>
+          <img class="display-image" src="${element.image}" alt="">
+        </ul>`
+        );
+      });
+      console.log(data.amiibo[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 }
+search();
+
+//Thami sucks
